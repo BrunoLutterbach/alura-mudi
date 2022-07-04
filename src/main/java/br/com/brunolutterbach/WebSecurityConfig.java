8e870e5fcccd37f733b389302b53c8141b996043
Bruno <brunolutterbach13@gmail.com>
2022-07-04 19:54:08 -0300

    Criado configuração para o Spring Security

---
+++ src/main/java/br/com/brunolutterbach/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.brunolutterbach;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Esse é um método que é chamado pelo Spring Security para configurar o filtro de segurança.
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated() // Todas as requisições devem ser autenticadas.
+                .and()
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Cria um usuário padrão para o Spring Security.
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("bruno")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
+
