df2108e21b7649da304d62a3b4cde19458abc964
Bruno <brunolutterbach13@gmail.com>
2022-07-05 18:41:38 -0300

    Classe User

---
+++ src/main/java/br/com/brunolutterbach/model/User.java
@@ -0,0 +1,24 @@
+package br.com.brunolutterbach.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String password;
+    private Boolean enable;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Pedido> pedidos;
+
+}
