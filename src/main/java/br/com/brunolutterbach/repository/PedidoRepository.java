6939e577c90542889cf0646f3d5a29af426e302d
Bruno <brunolutterbach13@gmail.com>
2022-07-05 18:41:18 -0300

    Adicionados métodos para buscar pedido do usuário logado e por status

--- src/main/java/br/com/brunolutterbach/repository/PedidoRepository.java
+++ src/main/java/br/com/brunolutterbach/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package br.com.brunolutterbach.repository;
 import br.com.brunolutterbach.model.Pedido;
 import br.com.brunolutterbach.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,9 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
     List<Pedido> findByStatus(StatusPedido status);
 
+    @Query("select p from Pedido p join p.user u where u.username = :username")
+    List<Pedido> findAllByUsuario(@Param("username") String username);
+
+    @Query("select p from Pedido p join p.user u where u.username = :username and p.status = :status")
+    List<Pedido> findByStatusAndUsuario(StatusPedido status,@Param("username") String username);
 }
